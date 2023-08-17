4fa886977d626fb1368cb652f517bfb4fd85d8df
Utpal Singh <utpalsingh915@gmail.com>
2023-08-17 15:26:32 +0530

    career page working done

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './App.css'
-import { ToastContainer } from "react-toast";
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./App.css";
+import { Toaster } from "react-hot-toast";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <ToastContainer delay={3000} position="top-center" />
+    <Toaster position="top-center"  />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/schemas/index.jsx
+++ src/schemas/index.jsx
@@ -1,4 +1,4 @@
-import * as Yup from "yup";
+import * as Yup from "Yup";
 
 export const sendMessageDto = Yup.object({
   name: Yup.string().required("Name is a required field"),
@@ -7,4 +7,38 @@ export const sendMessageDto = Yup.object({
     .required("Please enter your email."),
   phone: Yup.string().required("Please enter your phone number."),
   message: Yup.string().required("Please enter your message."),
+});
+
+export const agentApplicationSchema = Yup.object().shape({
+  title: Yup.string().required("Please Select a title"),
+  firstName: Yup.string().required("Please Enter Your First Name"),
+  lastName: Yup.string().required("Please Enter Your First Name"),
+  email: Yup.string().email().required("Please Enter Your Email Address"),
+  phone: Yup.string().required("Please Enter Your Phone Number"),
+  role: Yup.string()
+    .oneOf(["AGENT", "DEALERSHIP", "FEILDOFFICER"])
+    .required("Please Select a Post"),
+  city: Yup.string().required("Please Enter Your City"),
+  resume: Yup.mixed()
+    .test(
+      "fileType",
+      "Invalid file format. Only accept PDF or DOC files.",
+      (value) => {
+        if (!value) return true;
+        return [
+          "application/pdf",
+          "application/msword",
+          "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        ].includes(value.type);
+      }
+    )
+    .test(
+      "fileSize",
+      "File size is too large. Maximum size allowed is 2MB.",
+      (value) => {
+        if (!value) return true;
+        return value.size <= 2 * 1024 * 1024;
+      }
+    )
+    .required("Please Upload Your Resume"),
 });
