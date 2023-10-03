55f04bebf179ccd67d4d765a50a5f0ffd0094385
Utpal Singh <utpalsingh915@gmail.com>
2023-10-03 21:54:19 +0530

    about fixed

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -427,7 +427,7 @@ const Homepage = () => {
                     </p>
                     <div className="flex items-start justify-center mt-4 w-full">
                       <Link
-                        to="/about-us"
+                        to="/apply-loan"
                         className="pushable rounded-3xl  bg-orange-400 hover:bg-orange-700 hover:text-blue-900 transform-cpu"
                       >
                         <span className="front bg-gray-400 px-4 py-2  rounded-3xl font-semibold">
