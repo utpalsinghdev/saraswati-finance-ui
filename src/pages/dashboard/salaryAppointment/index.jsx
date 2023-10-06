3e385cbd5cd79dadaace4f0d822a3abb17a3552b
Utpal Singh <utpalsingh915@gmail.com>
2023-10-06 21:29:01 +0530

    viewer removed

--- src/pages/dashboard/salaryAppointment/index.jsx
+++ src/pages/dashboard/salaryAppointment/index.jsx
@@ -1437,9 +1437,7 @@ export default function Appointment() {
   ) : (
     <>
       {renderModal()}
-      <PDFViewer height={1000} width={600}>
-        <PdfFile data={agents.data[agents.data.length - 1]} />
-      </PDFViewer>
+
       <ConfirmationModal
         description="Do you really want to delete this This Appointment letter ?"
         isDelete
