c98365b98f8cdcc0a53cadfbcf8277d9823e0a98
Utpal Singh <utpalsingh915@gmail.com>
2023-10-14 01:17:55 +0530

    removing bars

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -2,9 +2,6 @@ import { Fragment, useState } from "react";
 import { Dialog, Menu, Transition } from "@headlessui/react";
 import {
   Bars3Icon,
-  CalendarIcon,
-  ChartPieIcon,
-  Cog6ToothIcon,
   DocumentDuplicateIcon,
   FolderIcon,
   HomeIcon,
@@ -484,20 +481,24 @@ export default function DashboardLayout({ children }) {
 
         <div className="lg:pl-72">
           <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
-            <button
-              type="button"
-              className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
-              onClick={() => setSidebarOpen(true)}
-            >
-              <span className="sr-only">Open sidebar</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-            </button>
+            {user && user.role !== "ADMIN" ? null : (
+              <>
+                <button
+                  type="button"
+                  className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+                  onClick={() => setSidebarOpen(true)}
+                >
+                  <span className="sr-only">Open sidebar</span>
+                  <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                </button>
 
-            {/* Separator */}
-            <div
-              className="h-6 w-px bg-gray-900/10 lg:hidden"
-              aria-hidden="true"
-            />
+                {/* Separator */}
+                <div
+                  className="h-6 w-px bg-gray-900/10 lg:hidden"
+                  aria-hidden="true"
+                />
+              </>
+            )}
 
             <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
               <form className="relative flex flex-1" action="#" method="GET">
