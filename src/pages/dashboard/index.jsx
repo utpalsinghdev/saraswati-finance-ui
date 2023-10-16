98762fa9beeb6f545bc8fcdf4c57c16e2e2e745c
Utpal Singh <utpalsingh915@gmail.com>
2023-10-16 12:42:22 +0530

    fixed

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -481,7 +481,7 @@ export default function DashboardLayout({ children }) {
 
         <div className="lg:pl-72">
           <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
-            {user && user.role !== "ADMIN" ? null : (
+            {user && user.user.role !== "ADMIN" ? null : (
               <>
                 <button
                   type="button"
