a665368834f5a097af3c7efed9270f1f8c90fbc0
Utpal Singh <utpalsingh915@gmail.com>
2023-10-29 17:48:48 +0530

    done

--- src/pages/dashboard/approvalLetter/index.jsx
+++ src/pages/dashboard/approvalLetter/index.jsx
@@ -1815,7 +1815,9 @@ export default function ApprovalLetter() {
   ) : (
     <>
       {renderModal()}
-
+      {/* <PDFViewer height={1000} width={600}>
+        <PdfFile data={agents.data[0]} />
+      </PDFViewer> */}
       <ConfirmationModal
         description="Do you really want to delete this This letter?"
         isDelete
