aaaa992b1b3adf5e34d5bec52186833a78de1542
Utpal Singh <utpalsingh915@gmail.com>
2023-12-10 23:11:31 +0530

    Fix PDFViewer rendering issue

--- src/pages/dashboard/approvalLetter/index.jsx
+++ src/pages/dashboard/approvalLetter/index.jsx
@@ -1848,9 +1848,9 @@ export default function ApprovalLetter() {
   ) : (
     <>
       {renderModal()}
-      <PDFViewer height={1000} width={600}>
+      {/* <PDFViewer height={1000} width={600}>
         <PdfFile data={agents.data[1]} />
-      </PDFViewer>
+      </PDFViewer> */}
       <ConfirmationModal
         description="Do you really want to delete this This letter?"
         isDelete
