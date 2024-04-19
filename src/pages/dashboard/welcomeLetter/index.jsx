25cf9d5a2a03f0c7c309bcf9877782281c333646
Utpal Singh <utpalsingh915@gmail.com>
2024-04-19 08:10:06 +0530

    Remove commented out code for PDFViewer

--- src/pages/dashboard/welcomeLetter/index.jsx
+++ src/pages/dashboard/welcomeLetter/index.jsx
@@ -1749,9 +1749,9 @@ function WelcomeLetter() {
   ) : (
     <>
       {renderModal()}
-      <PDFViewer height={1000} width={600}>
+      {/* <PDFViewer height={1000} width={600}>
         <PdfFile data={agents.data[0]} />
-      </PDFViewer>
+      </PDFViewer> */}
       <ConfirmationModal
         description="Do you really want to delete this letter?"
         isDelete
