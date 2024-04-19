8be4e2bb40559db659586acbb25695670c0c1243
Utpal Singh <utpalsingh915@gmail.com>
2024-04-19 06:59:11 +0530

    Update user authentication logic

--- src/pages/dashboard/payment/Qr.jsx
+++ src/pages/dashboard/payment/Qr.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import useFetch from "../../../hooks/useFetch";
 import { PhotoIcon } from "@heroicons/react/24/outline";
 
-import { useFormik } from "formik";
+import { Formik, useFormik } from "formik";
 import toast from "react-hot-toast";
 import ApiService from "../../../services/Api_services";
 import Button from "../../../components/ui/button";
@@ -68,169 +68,258 @@ function Qr() {
 
     reader.readAsDataURL(file);
   }
-  const {
-    values,
-    handleSubmit,
-    isSubmitting,
-    errors,
-    handleBlur,
-    touched,
-    handleChange,
-  } = useFormik({
-    initialValues: formData,
-    enableReinitialize: true,
-    validate: (values) => {
-      return errors;
-    },
-    onSubmit: async (values, action) => {
-      const payload = {
-        ...values,
-      };
+  // const {
+  //   values,
+  //   handleSubmit,
+  //   isSubmitting,
+  //   errors,
+  //   handleBlur,
+  //   touched,
+  //   handleChange,
+  // } = useFormik({
+  //   initialValues: formData,
+  //   enableReinitialize: true,
+  //   validate: (values) => {
+  //     return errors;
+  //   },
+  //   onSubmit: async (values, action) => {
+  //     const payload = {
+  //       ...values,
+  //     };
 
-      await new Promise((resolve) => {
-        fileToBase64(values.file, (base64Data) => {
-          payload.qr = base64Data;
-          resolve();
-        });
-      });
-      try {
-        const res = await ApiService.fetchData({
-          url: `api/payment-qr`,
-          method: "POST",
-          data: payload,
-        });
-        toast.success(res.data.message);
-        fetchData();
-      } catch (error) {
-        toast.error(
-          typeof error.response.data.message !== "string"
-            ? error.response.data?.[0]
-            : error.response.data.message
-        );
-      } finally {
-        action.resetForm();
-        action.setSubmitting(false);
-      }
-    },
-  });
+  //     await new Promise((resolve) => {
+  //       fileToBase64(values.file, (base64Data) => {
+  //         payload.qr = base64Data;
+  //         resolve();
+  //       });
+  //     });
+  //     try {
+  //       const res = await ApiService.fetchData({
+  //         url: `api/payment-qr`,
+  //         method: "POST",
+  //         data: payload,
+  //       });
+  //       toast.success(res.data.message);
+  //       fetchData();
+  //     } catch (error) {
+  //       toast.error(
+  //         typeof error.response.data.message !== "string"
+  //           ? error.response.data?.[0]
+  //           : error.response.data.message
+  //       );
+  //     } finally {
+  //       action.resetForm();
+  //       action.setSubmitting(false);
+  //     }
+  //   },
+  // });
   return (
     <div className="w-full  flex flex-col">
       <h1>
         {general.data
-          ? `Last Updated on ${moment(general.data.createdAt).format("ll")}`
+          ? `Last Updated on ${moment(general.data.updatedAt).format("ll")}`
           : "No Qr Code"}
       </h1>
       <h1 className=" text-xl font-bold  ">Pyament Qr Code</h1>
-      <form
-        onSubmit={handleSubmit}
-        className="flex relative items-center justify-center w-full flex-col h-[30vh]"
+      <Formik
+        initialValues={formData}
+        enableReinitialize={true}
+        onSubmit={async (values, action) => {
+          const payload = {
+            ...values,
+          };
+          await new Promise((resolve) => {
+            fileToBase64(values.file, (base64Data) => {
+              payload.qr = base64Data;
+              resolve();
+            });
+          });
+          try {
+            const res = await ApiService.fetchData({
+              url: `api/payment-qr`,
+              method: "POST",
+              data: payload,
+            });
+            toast.success(res.data.message);
+            fetchData();
+          } catch (error) {
+            toast.error(
+              typeof error.response.data.message !== "string"
+                ? error.response.data?.[0]
+                : error.response.data.message
+            );
+          } finally {
+            action.resetForm();
+            action.setSubmitting(false);
+          }
+        }}
       >
-        <div className="w-[50%]">
-          <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-            <div className="text-center">
-              <PhotoIcon
-                className="mx-auto h-12 w-12 text-gray-300"
-                aria-hidden="true"
-              />
-              <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                <label
-                  htmlFor="file-upload"
-                  className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-                >
-                  <span>
-                    {values.file.name ? values.file.name : "Upload a file"}
-                  </span>
-                  <input
-                    accept="image/*"
-                    id="file-upload"
-                    name="file"
-                    type="file"
-                    onBlur={handleBlur}
-                    onChange={(e) => {
-                      values.file = e.target.files[0];
-                    }}
-                    className="sr-only"
+        {({
+          values,
+          handleSubmit,
+          isSubmitting,
+          errors,
+          handleBlur,
+          touched,
+          handleChange,
+        }) => (
+          <form
+            onSubmit={handleSubmit}
+            className="flex relative items-center justify-center w-full flex-col h-[30vh]"
+          >
+            <div className="w-[50%]">
+              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                <div className="text-center">
+                  <PhotoIcon
+                    className="mx-auto h-12 w-12 text-gray-300"
+                    aria-hidden="true"
                   />
-                </label>
+                  <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                    <label
+                      htmlFor="file-upload"
+                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                    >
+                      <span>
+                        {values.file.name ? values.file.name : "Upload a file"}
+                      </span>
+                      <input
+                        accept="image/*"
+                        id="file-upload"
+                        name="file"
+                        type="file"
+                        onBlur={handleBlur}
+                        onChange={(e) => {
+                          values.file = e.target.files[0];
+                        }}
+                        className="sr-only"
+                      />
+                    </label>
+                  </div>
+                  <p className="text-xs leading-5 text-gray-600">
+                    PNG, JPG, GIF up to 2MB
+                  </p>
+                </div>
               </div>
-              <p className="text-xs leading-5 text-gray-600">
-                PNG, JPG, GIF up to 2MB
-              </p>
             </div>
-          </div>
-        </div>
-        <span>{touched.file && errors.file}</span>
-        <Button disabled={isSubmitting} type={"submit"} className={"w-max"}>
-          {isSubmitting ? "Uploading..." : "Upload"}
-        </Button>
-      </form>
+            <span>{touched.file && errors.file}</span>
+            <Button disabled={isSubmitting} type={"submit"} className={"w-max"}>
+              {isSubmitting ? "Uploading..." : "Upload"}
+            </Button>
+          </form>
+        )}
+      </Formik>
+
       <h1 className=" text-xl font-bold">Bank Details & file Charge</h1>
-      <form
-        // onSubmit={f.handleSubmit}
-        className="relative pt-4 rounded-b-md pb-8 border-t border-gray-300 flex flex-col gap-4 px-4 bg-white"
+      <Formik
+        initialValues={formData}
+        enableReinitialize={true}
+        onSubmit={async (values, action) => {
+          const payload = {
+            ...values,
+          };
+          await new Promise((resolve) => {
+            fileToBase64(values.file, (base64Data) => {
+              payload.qr = base64Data;
+              resolve();
+            });
+          });
+          try {
+            const res = await ApiService.fetchData({
+              url: `api/payment-qr`,
+              method: "POST",
+              data: payload,
+            });
+            toast.success(res.data.message);
+            fetchData();
+          } catch (error) {
+            toast.error(
+              typeof error.response.data.message !== "string"
+                ? error.response.data?.[0]
+                : error.response.data.message
+            );
+          } finally {
+            action.resetForm();
+            action.setSubmitting(false);
+          }
+        }}
       >
-        <div className="max-w-md">
-          {" "}
-          <Input
-            name="bankName"
-            type={"text"}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.bankName}
-            error={touched.location && errors.bankName}
-            icon={<Building2Icon size={18} className=" text-indigo-500" />}
-            label={"Bank Name"}
-            placeholder={"Enter Bank Name"}
-          />
-          <Input
-            name="accountNo"
-            type={"text"}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.accountNo}
-            error={touched.accountNo && errors.accountNo}
-            icon={<Building2Icon size={18} className=" text-indigo-500" />}
-            label={"Account Number"}
-            placeholder={"Enter Account Number"}
-          />
-          <Input
-            name="ifsc"
-            type={"text"}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.ifsc}
-            error={touched.ifsc && errors.ifsc}
-            icon={<Building2Icon size={18} className=" text-indigo-500" />}
-            label={"IFSC"}
-            placeholder={"Enter IFSC"}
-          />
-          <Input
-            name="holderName"
-            type={"text"}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.holderName}
-            error={touched.holderName && errors.holderName}
-            icon={<Building2Icon size={18} className=" text-indigo-500" />}
-            label={"Account Holder Name"}
-            placeholder={"Enter Account Holder Name"}
-          />
-          <Input
-            name="fileCharge"
-            type={"text"}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.fileCharge}
-            error={touched.fileCharge && errors.fileCharge}
-            icon={<Building2Icon size={18} className=" text-indigo-500" />}
-            label={"File Charge"}
-            placeholder={"Enter File Charge"}
-          />
-        </div>
-        <Button disabled={isSubmitting} type={"submit"} className={"w-max"}>
-          {isSubmitting ? "Uploading..." : "Upload"}
-        </Button>
-      </form>
+        {({
+          values,
+          handleSubmit,
+          isSubmitting,
+          errors,
+          handleBlur,
+          touched,
+          handleChange,
+        }) => (
+          <form
+            // onSubmit={f.handleSubmit}
+            className="relative pt-4 rounded-b-md pb-8 border-t border-gray-300 flex flex-col gap-4 px-4 bg-white"
+          >
+            <div className="max-w-md">
+              {" "}
+              <Input
+                name="bankName"
+                type={"text"}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.bankName}
+                error={touched.location && errors.bankName}
+                icon={<Building2Icon size={18} className=" text-indigo-500" />}
+                label={"Bank Name"}
+                placeholder={"Enter Bank Name"}
+              />
+              <Input
+                name="accountNo"
+                type={"text"}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.accountNo}
+                error={touched.accountNo && errors.accountNo}
+                icon={<Building2Icon size={18} className=" text-indigo-500" />}
+                label={"Account Number"}
+                placeholder={"Enter Account Number"}
+              />
+              <Input
+                name="ifsc"
+                type={"text"}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.ifsc}
+                error={touched.ifsc && errors.ifsc}
+                icon={<Building2Icon size={18} className=" text-indigo-500" />}
+                label={"IFSC"}
+                placeholder={"Enter IFSC"}
+              />
+              <Input
+                name="holderName"
+                type={"text"}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.holderName}
+                error={touched.holderName && errors.holderName}
+                icon={<Building2Icon size={18} className=" text-indigo-500" />}
+                label={"Account Holder Name"}
+                placeholder={"Enter Account Holder Name"}
+              />
+              <Input
+                name="fileCharge"
+                type={"text"}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.fileCharge}
+                error={touched.fileCharge && errors.fileCharge}
+                icon={<Building2Icon size={18} className=" text-indigo-500" />}
+                label={"File Charge"}
+                placeholder={"Enter File Charge"}
+              />
+            </div>
+            <Button disabled={isSubmitting} type={"submit"} className={"w-max"}>
+              {isSubmitting ? "Uploading..." : "Upload"}
+            </Button>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 }
