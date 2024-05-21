b28e804412c114ba817850d4a440b78bfc136ec3
Utpal Singh <utpalsingh915@gmail.com>
2024-05-21 11:41:06 +0530

    Update copyright notice in PostFooter component

--- src/components/Footer/PostFooter.jsx
+++ src/components/Footer/PostFooter.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import metaData from "../../utils/lib/site.config";
 
 function PostFooter() {
   const currentYear = new Date().getFullYear();
   return (
     <div className="container mx-auto w-[90%] flex justify-between items-center py-4 text-black">
       <span>
-        All Right Reserved | Copyright &copy; {currentYear} Fundwisor Finance
-        Business Solution Services Private Limited.
+        All Right Reserved | Copyright &copy; {currentYear} {metaData.title}
       </span>
     </div>
   );
