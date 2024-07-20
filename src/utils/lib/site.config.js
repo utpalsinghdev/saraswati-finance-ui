49c8114ac630443d2238ca9c2e2cbdda1b4340f0
utpal singh <utpalsingh915@gmail.com>
2024-07-20 17:33:59 +0530

    Update phone number in site.config.js

--- src/utils/lib/site.config.js
+++ src/utils/lib/site.config.js
@@ -1,12 +1,11 @@
 const metaData = {
-  title: "LAKSHMI FINANCE PVT. LTD.",
-  siteUrl: "https://lakshmifinance.live",
-  email: "info@lakshmifinance.live",
-  phone: ["8859769434", "8392908548"],
-  address:
-    "Plot No. 235 A Ground Floor Sudham Corner Of 27th and 28th RD Off linking Road Bandra West Mumbai Maharashtra Pin- 400050",
-  fileCharge: "4150",
-  icardIsPdf: false,
+    title: "LAKSHMI FINANCE PVT. LTD.",
+    siteUrl: "https://lakshmifinance.live",
+    email: "info@lakshmifinance.live",
+    phone: ["8392908548"],
+    address: "Plot No. 235 A Ground Floor Sudham Corner Of 27th and 28th RD Off linking Road Bandra West Mumbai Maharashtra Pin- 400050",
+    fileCharge: "4150",
+    icardIsPdf: false,
 };
 
 export default metaData;
