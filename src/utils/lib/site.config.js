8ed14547e889d0ede006527766c4ca23cde05450
Utpal Singh <utpalsingh915@gmail.com>
2024-09-02 20:16:35 +0530

    feat: Update registered office address in site.config.js

--- src/utils/lib/site.config.js
+++ src/utils/lib/site.config.js
@@ -6,7 +6,7 @@ const metaData = {
     address: [
         "Corporate Office : FP-30B PITAMPURA ,NEAR TV  TOWER, PRITAM PURA Delhi, 110034",
         "Corporate Office - Office no. 305,309 Balaji complex Vadodara Gujrat Pincode - 380012",
-        "(Registered Office Address - Vasundhara Building, 2nd floor, space No-5 & 6, 2/7, Sarat bose road, Kolkata, West Bengal, Pincode - 700020)",
+        "Registered Office Address - Vasundhara Building, 2nd floor, space No-5 & 6, 2/7, Sarat bose road, Kolkata, West Bengal, Pincode - 700020",
         "Head Office - Arulmigu Sri Muppathamman Building 3rd floor Thyagaraya Nagar Chennai Pincode - 600017"
     ],
     fileCharge: "4550",
