3e0d2a0cb85afba03e6f200df47a28592d5b0384
Utpal Singh <utpalsingh915@gmail.com>
2024-11-07 11:32:05 +0530

    feat: Update registered office address in site.config.js

--- src/utils/lib/site.config.js
+++ src/utils/lib/site.config.js
@@ -1,13 +1,10 @@
 const metaData = {
     title: "Supriya financial services pvt ltd",
-    siteUrl: "https://supriyafinancial.com",
-    email: "info@supriyafinancial.com",
-    phone: ["7060099906", "7618331738", "7579611981"],
+    siteUrl: "https://supriyafinancial.in",
+    email: "info@supriyafinancial.in",
+    phone: ["9266283114", "9266280572"],
     address: [
-        "Corporate Office : FP-30B PITAMPURA ,NEAR TV  TOWER, PRITAM PURA Delhi, 110034",
-        "Corporate Office - Office no. 305,309 Balaji complex Vadodara Gujrat Pincode - 380012",
-        "Registered Office Address - Vasundhara Building, 2nd floor, space No-5 & 6, 2/7, Sarat bose road, Kolkata, West Bengal, Pincode - 700020",
-        "Head Office - Arulmigu Sri Muppathamman Building 3rd floor Thyagaraya Nagar Chennai Pincode - 600017"
+        "Address: Malik Buildcon Plaza-1, F-2, First Floor Plot-05 Sector 4 Road, Pocket 2, Sector 12 Dwarka, Delhi, 110075",
     ],
     fileCharge: "4550",
     icardIsPdf: true,
