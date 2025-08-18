3a275772780cf82d99c812c3987054a55d803bd7
utpal singh <utpalsingh915@gmail.com>
2025-08-18 18:12:26 +0530

    refactor: Update site configuration with new URL, email, and phone number for improved accuracy

--- src/utils/lib/site.config.js
+++ src/utils/lib/site.config.js
@@ -1,14 +1,14 @@
 const metaData = {
-    title: "Saraswati Financial Services Private  Limited",
-    siteUrl: "https://www.saraswati-financial.com",
-    email: "info@saraswati-financial.com",
-    phone: ["875594xxxx"],
-    address: [
-        "Head Office- Plot no -258, K.V Nagar, Unit-3, Kharavela Nagar, Distt- Khorda (Orissa) 751001",
-        "Corporate Office- Tower A- 317 , 3rd floor, Bestech Business Tower, Sector-66, SAS Nagar, Chandigarh(Punjab) 160066",
-    ],
-    fileCharge: "4550",
-    icardIsPdf: true,
+  title: "Saraswati Financial Services Private  Limited",
+  siteUrl: "https://www.saraswatifinance.live",
+  email: "Info@Saraswatifinance.live",
+  phone: ["9773945780"],
+  address: [
+    "Head Office- Plot no -258, K.V Nagar, Unit-3, Kharavela Nagar, Distt- Khorda (Orissa) 751001",
+    "Corporate Office- Tower A- 317 , 3rd floor, Bestech Business Tower, Sector-66, SAS Nagar, Chandigarh(Punjab) 160066",
+  ],
+  fileCharge: "4550",
+  icardIsPdf: true,
 };
 
 export default metaData;
